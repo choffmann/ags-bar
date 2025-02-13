31314798b06bd3b399afd39e07bbceb901127b68
Cedrik Hoffmann <dev@choffmann.io>
2025-02-14 00:42:36 +0100

    feat: display first widget

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
+local scss = "./style.scss"
+local css = "/tmp/style.css"
+local astal = require("astal")
 local App = require("astal.gtk3.app")
 
+local Bar = require("widget.bar")
+
+astal.exec(string.format("sass %s %s", scss, css))
+
 App:start({
+	css = css,
 	main = function()
-		print("Hello world")
+		Bar(0)
+		Bar(1)
 	end,
 })
---
+++ widget/bar.lua
@@ -0,0 +1,23 @@
+local Widget = require("astal.gtk3.widget")
+local Anchor = require("astal.gtk3").Astal.WindowAnchor
+
+return function(monitor)
+	return Widget.Window({
+		class_name = "bar",
+		monitor = monitor,
+		anchor = Anchor.TOP + Anchor.LEFT + Anchor.RIGHT,
+		exclusivity = "EXCLUSIVE",
+		Widget.CenterBox({
+			class_name = "centerbox",
+			margin_start = 10,
+			margin_end = 10,
+			margin_top = 5,
+			center_widget = Widget.Box({
+				Widget.Label({
+					class_name = "box p-1",
+					label = "Foo Bar",
+				}),
+			}),
+		}),
+	})
+end
